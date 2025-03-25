674496d57391ca436cf0766e9e4fa1777a4e9ff2
Marcelo Carvalho <163018551+marcelojrac@users.noreply.github.com>
2025-03-25 11:29:32 +0000

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# glanceweb2
- 
+# DTools
---
+++ gaze/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import vhtml from 'vhtml';
+import { REVISION } from 'three';
+
+/** @jsx vhtml */
+
+export function Footer() {
+	return (
+		<footer>
+			<a class="item" target="_blank" href="https://threejs.org/">
+				three.js r{REVISION}
+			</a>
+		</footer>
+	);
+}
